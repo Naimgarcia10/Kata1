13d75bdd19acfc3e4a7e418ffef9a04c2b0c3b87
Entrar <Entrar@10.22.147.52>
2022-09-14 10:01:00 +0100

    Second Commit

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,16 @@
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Jose", new Date (102, 9, 24));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002,10,24);
+        Person person = new Person("Jose", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años.");
     }
 
